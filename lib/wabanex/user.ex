6f11c614be2637e0bdfb9ab3fb8452bc242821e6
Hugo <hugolacerdar@gmail.com>
2021-06-22 19:14:23 -0300

    Add user schema

---
+++ lib/wabanex/user.ex
@@ -0,0 +1,25 @@
+defmodule Wabanex.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @required_keys [:email, :name, :password]
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_keys)
+    |> validate_required(@required_keys)
+    |> validate_length(:name, min: 2, max: 30)
+    |> validate_length(:password, min: 8, max: 30)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+  end
+end
