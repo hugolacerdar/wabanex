2d642e674bad2f2e1bc7fbd380a0eb9490778601
Hugo <hugolacerdar@gmail.com>
2021-06-25 12:24:05 -0300

    Add proloading current training

--- lib/wabanex/users/get.ex
+++ lib/wabanex/users/get.ex
@@ -1,10 +1,21 @@
 defmodule Wabanex.Users.Get do
-  alias Wabanex.{Repo, User}
+  import Ecto.Query
+  alias Wabanex.{Repo, Training, User}
 
   def by_id(id) do
     case Repo.get(User, id) do
       nil -> {:error, "User not found"}
-      user -> {:ok, user}
+      user -> {:ok, load_training(user)}
     end
+  end
+
+  defp load_training(user) do
+    today = Date.utc_today()
+
+    query = from t in Training,
+            where: ^today >= t.start_date
+                    and ^today <= t.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
